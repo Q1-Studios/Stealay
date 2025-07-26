e5771a3428112fe179791f029f2bf744d714064d
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-07-27 00:58:33 +0200

    Don't allow sliding into a button press

--- scripts/click_darken.gd
+++ scripts/click_darken.gd
@@ -1,16 +1,10 @@
 extends Sprite2D
 
-var _mouse_inside: bool = false
+
+@export var btn_src: TouchControl
 
 func _process(_delta: float) -> void:
-	if _mouse_inside and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if btn_src.pressed:
 		modulate.v = 0.5
 	else:
 		modulate.v = 1
-
-func _on_mouse_entered() -> void:
-	_mouse_inside = true
-	
-
-func _on_mouse_exited() -> void:
-	_mouse_inside = true
--- scripts/touch_control_area2d.gd
+++ scripts/touch_control_area2d.gd
@@ -5,13 +5,20 @@ var pressed: bool = false
 var just_pressed: bool = false
 var just_released: bool = false
 var _mouse_inside: bool = false
+var _block_pressed : bool = false
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 
 func _process(_delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and _mouse_inside:
+	# Don't allow "sliding" into a button - a button must be directly pressed
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not _mouse_inside:
+		_block_pressed = true
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		_block_pressed = false
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and _mouse_inside and not _block_pressed:
 		if not pressed:
 			just_pressed = true
 		else:
